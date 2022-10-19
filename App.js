f91c711e0abb1c480d5dee27830bebebbee2d45d
Alfredo Tiapa <57116402+crypto-verga@users.noreply.github.com>
2022-10-19 09:14:33 -0400

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react'
+// import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import _raw from './data.xlsx'; //este archivo esta en mi ubicacion, contiene el texto "Hola Mundo!!..."
+import * as XLSX from 'xlsx/xlsx.mjs';
 
 export default function App() {
+  const [raw, setRaw] = useState();
+  
+  fetch(_raw).then(res => { 
+    return res.arrayBuffer();
+  }).then(res => {
+    const workbook = XLSX.read(new Uint8Array(res), {
+        type: 'array'
+    });
+    const workBookSheets = workbook.SheetNames
+    const datos =  XLSX.utils.sheet_to_json(workbook.Sheets[workBookSheets[0]])
+    setRaw(datos)
+  })
+  
+  
+  console.log(raw)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <div className="App">
+      <div>
+        <h1></h1>
+      </div>
+    </div>
   );
 }
 
