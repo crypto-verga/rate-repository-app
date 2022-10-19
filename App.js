1f1962f10bd202f2b42d24ea20e6ae327288ace5
Alfredo Tiapa <57116402+crypto-verga@users.noreply.github.com>
2022-10-19 08:46:32 -0400

    fix details
    order functions and fixing errors

--- App.js
+++ App.js
@@ -1,32 +1,38 @@
 import React, {useState} from 'react'
-// import { StatusBar } from 'expo-status-bar'
+import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
-import _raw from './data.xlsx'; //este archivo esta en mi ubicacion, contiene el texto "Hola Mundo!!..."
-import * as XLSX from 'xlsx/xlsx.mjs';
+import _raw from './data.xlsx' //este archivo esta en mi ubicacion, contiene el texto "Hola Mundo!!..."
+import * as XLSX from 'xlsx/xlsx.mjs'
+
+const splitData = res => {
+  const workbook = XLSX.read(new Uint8Array(res), {
+      type: 'array'
+  });
+  const workBookSheets = workbook.SheetNames
+  const datos =  XLSX.utils.sheet_to_json(workbook.Sheets[workBookSheets[0]])
+
+  return datos
+}
+
+function exceldata(){
+  const [raw, setRaw] = useState([]);
+  
+  fetch(_raw)
+    .then(res => { return res.arrayBuffer() })
+    .then(setRaw(splitData))
+
+  return raw
+}
+
+console.log(exceldata())
 
 export default function App() {
-  const [raw, setRaw] = useState();
-  
-  fetch(_raw).then(res => { 
-    return res.arrayBuffer();
-  }).then(res => {
-    const workbook = XLSX.read(new Uint8Array(res), {
-        type: 'array'
-    });
-    const workBookSheets = workbook.SheetNames
-    const datos =  XLSX.utils.sheet_to_json(workbook.Sheets[workBookSheets[0]])
-    setRaw(datos)
-  })
-  
-  
-  console.log(raw)
   return (
-    <div className="App">
-      <div>
-        <h1></h1>
-      </div>
-    </div>
-  );
+    <View style={styles.container}>
+      <Text>Hola Pepe!</Text>
+      <StatusBar style='auto'/>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
